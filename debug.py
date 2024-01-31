b8f3aa748f105b9df311fe5702b0a075948593ba
Danu_Herath <117981871+DanushH@users.noreply.github.com>
2024-01-31 12:10:13 +0530

    debugging

---
+++ debug.py
@@ -0,0 +1,62 @@
+
+# 1. Compilation Errors
+
+# Example 1:
+# print("Hello world")
+#     print("Hi")
+
+# Example 2:
+# print(int(3.15)
+
+
+
+# 2. Runtime Errors
+
+# Example 1:
+# print(x)
+
+# Example 2:
+# print("123" + 4)
+# print(123 + "4")
+
+# Example 3:
+# num = int("hello")
+# print(num)
+
+# Example 4:
+# print(123 / 0)
+
+# Example 5:
+# square_numbers = [1, 4, 9, 16]
+# print(square_numbers[4])
+
+# Example 6:
+# def factorial(num):
+#     # base case
+#     # if num == 1:
+#     #     return 1
+
+#     # recursive case
+#     return num * factorial(num - 1)
+
+# NUM = 3
+# print(f"Factorial of {NUM} is {factorial(NUM)}")
+
+
+
+# 3. Logical Errors
+
+# Example 1:
+# for i in range(1, 5):
+#     print(i ** 2)
+
+# Example 2:
+# square_numbers = [1, 4, 9, 16, 25]
+# print(square_numbers[3])
+
+# Example 3:
+# original = [1, 4, 9]
+# copy = original    
+# original.append(16)
+
+# print(f"original is {original}, copy is {copy}")
